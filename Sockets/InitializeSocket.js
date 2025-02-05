158421037cd25dee01f196bf0a388a35c5e73cf0
Yashraj Mashruwala <105281067+Helix1579@users.noreply.github.com>
2025-02-05 13:30:56 +0100

    Socket Server Establised with frontend

---
+++ Sockets/InitializeSocket.js
@@ -0,0 +1,28 @@
+import { Server } from 'socket.io';
+
+export const InitializeSocket = (server) => {
+    console.log('Initializing WebSocket ...');
+    const io = new Server(server, {
+        cors: {
+            origin: '*', // Allow all origins, adjust as needed
+            methods: ['GET', 'POST']
+        }
+    });
+
+    // Handle WebSocket connection
+    io.on('connection', (socket) => {
+        console.log('a user connected');
+
+        // Listen for custom events
+        socket.on('message', (msg) => {
+            console.log('Received message:', msg);
+            socket.emit('message', 'Message received: ' + msg); // Example response
+        });
+
+        socket.on('disconnect', () => {
+            console.log('a user disconnected');
+        });
+    });
+
+    return io;
+};
