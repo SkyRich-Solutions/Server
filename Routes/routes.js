2d2166470c6ce47aca340f855ba66a61fe20c2b0
Yashraj Mashruwala <105281067+Helix1579@users.noreply.github.com>
2025-03-19 14:42:21 +0100

    Scripts are now triggered from the server via api call

--- Routes/routes.js
+++ Routes/routes.js
@@ -1,19 +1,32 @@
-import express from "express";
-import { upload, uploadCSV , getUnprocessedData} from "../Controller/UploadController.js";
-import { uploadCleanedData, getPredictionData } from "../Controller/processed controller/DataController.js";
+import express from 'express';
+import {
+    upload,
+    uploadCSV,
+    getUnprocessedData
+} from '../Controller/UploadController.js';
+import {
+    uploadCleanedData,
+    getPredictionData
+} from '../Controller/ProcessedController/DataController.js';
+import { ScriptController } from '../Controller/Script/ScriptController.js';
 
 const router = express.Router();
 
 // Initialize express router
 
-router.get("/", (req, res) => {
-    res.send("🚀 API Running!");
+router.get('/', (req, res) => {
+    res.send('🚀 API Running!');
 });
 
-
-router.post("/uploadFile", upload.single('file'), uploadCSV);
-router.get('/unprocessedData', getUnprocessedData);
-router.post('/uploadData', uploadCleanedData);
+// Front end routes
 router.get('/uploadPredictionData', getPredictionData);
+router.post('/uploadFile', upload.single('file'), uploadCSV);
+
+// Backend routes
+router.post('/uploadData', uploadCleanedData);
+router.post('/run-python', ScriptController);
+
+// Common routes
+router.get('/unprocessedData', getUnprocessedData);
 
 export default router;
