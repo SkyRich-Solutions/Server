ab753397a26960fd8048cc57f03286fe28e3e519
Yashraj Mashruwala <105281067+Helix1579@users.noreply.github.com>
2025-03-31 16:30:32 +0200

    Update routes.js

--- Routes/routes.js
+++ Routes/routes.js
@@ -2,7 +2,6 @@ import express from "express";
 import {
     upload,
     uploadCSV,
-    getUnprocessedData, 
     getUnprocessedMaterialData, 
     getUnprocessedTurbineData
 } from '../Controller/UploadController.js';
